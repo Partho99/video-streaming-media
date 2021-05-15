04ef2ed1eb52ac2a187cc65940c5403ede45d75e
Partho Das <partho.swe@gmail.com>
2021-05-16 01:24:16 +0600

    updated main index js file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import {render} from 'react-dom';
 import App from './app';
+import GlobalStyles from './global-styles'
+import 'normalize.css';
 
-render(<App/>, document.getElementById('root'));
+render(<>
+        <GlobalStyles/>
+        <App/>
+    </>,
+    document.getElementById('root')
+);
 
