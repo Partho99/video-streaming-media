c57ce0866bd9d28d31a07d322f5707e3932650e8
Partho Das <partho.swe@gmail.com>
2021-05-16 01:27:08 +0600

    added footer header jumbotron to the container

---
+++ src/containers/footer.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import {Footer} from "../components";
+
+export const FooterContainer = () => {
+    return (
+        <Footer>
+            <Footer.Title>Question? Contact us.</Footer.Title>
+            <Footer.Break/>
+            <Footer.Row>
+                <Footer.Column>
+                    <Footer.Link href='#'>FAQ</Footer.Link>
+                    <Footer.Link href='#'>Investor Relation</Footer.Link>
+                    <Footer.Link href='#'>Ways to Watch</Footer.Link>
+                    <Footer.Link href='#'>Corporate Information</Footer.Link>
+                    <Footer.Link href='#'>Netflix Originals</Footer.Link>
+                </Footer.Column>
+                <Footer.Column>
+                    <Footer.Link href='#'>Help Center</Footer.Link>
+                    <Footer.Link href='#'>Jobs</Footer.Link>
+                    <Footer.Link href='#'>Terms of Use</Footer.Link>
+                    <Footer.Link href='#'>Contact Us</Footer.Link>
+                    <Footer.Link href='#'>Netflix Originals</Footer.Link>
+                </Footer.Column>
+                <Footer.Column>
+                    <Footer.Link href='#'>Account</Footer.Link>
+                    <Footer.Link href='#'>Redeem Gift Cards</Footer.Link>
+                    <Footer.Link href='#'>Privacy</Footer.Link>
+                    <Footer.Link href='#'>Speed Test</Footer.Link>
+                    <Footer.Link href='#'>Netflix Originals</Footer.Link>
+                </Footer.Column>
+                <Footer.Column>
+                    <Footer.Link href='#'>Media center</Footer.Link>
+                    <Footer.Link href='#'>Buy Gift Cards</Footer.Link>
+                    <Footer.Link href='#'>Cookie Preference</Footer.Link>
+                    <Footer.Link href='#'>Legal Notice</Footer.Link>
+                    <Footer.Link href='#'>Netflix Originals</Footer.Link>
+                </Footer.Column>
+            </Footer.Row>
+            <Footer.Break/>
+            <Footer.Link href='#'>
+                <Footer.Text>Netflix Bangladesh</Footer.Text>
+            </Footer.Link>
+
+        </Footer>
+    )
+}
